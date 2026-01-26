2239f7ff1eabf308b9ed2e3142bb222fdf8a9215
todotoday <s260822@naver.com>
2026-01-27 01:23:01 +0900

    나무 정보 ui 수정

--- U-TreeMap-FrontEnd/tailwind.config.js
+++ U-TreeMap-FrontEnd/tailwind.config.js
@@ -1,7 +1,18 @@
 // tailwind.config.js
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
+  extend: {
+    fontFamily: {
+      sans: [
+        'Pretendard',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Noto Color Emoji',
+        'system-ui',
+        'sans-serif',
+      ],
+    },
   },
 };
