865035ff560de3af8adc096bde9bfdce8f2d9b55
OZVSHHH <dlgkdms4660@gmail.com>
2026-02-07 13:51:44 +0900

    add : svgr 라이브러리 추가

--- U-TreeMap-FrontEnd/vite.config.js
+++ U-TreeMap-FrontEnd/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),svgr()],
   server: {
     port: 5173,   // 원하는 포트 번호
     strictPort: true,   // 포트 막혀있으면 실행 x
